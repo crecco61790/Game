a6fff67458a2dca5140ee26af62dda2dc04a7605
Stephen Crecco <crecco61790@yahoo.com>
2018-02-15 10:46:17 -0500

    End Class 02/15/18

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.use(function (req, res, next) {
+  res.write('This is provided by newpaltz.edu\r\n');
+    next();
+})
+app.get('/hello', function (req, res) {
+    res.write('world');
+    res.end();
+  })
+app.get('/goodbye', function (req, res) {
+    res.write('New Paltz');
+    res.end();
+  })
+
+app.listen(8080);
