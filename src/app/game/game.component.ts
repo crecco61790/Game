c953d504baa1c73bf372834e1bef37cfd652e781
Stephen Crecco <crecco61790@yahoo.com>
2018-03-15 10:48:51 -0400

    Game UI

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+    Model = new Game
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
